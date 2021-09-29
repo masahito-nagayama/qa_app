b43232c602a0c13156c1ea9e491dbbfda4af082f
masahito-nagayama <me18sf67m@gmail.com>
2021-09-29 23:49:04 +0900

    add root method

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'questions#index'
+  get 'questions/show'
+  get 'questions/new'
+  get 'questions/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :questions
 end
