7672d064b0882283166c765520ce90bc676b2093
masahito-nagayama <me18sf67m@gmail.com>
2021-09-30 23:58:54 +0900

    add bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.create(id: 1, name: "Test name 1", title: " Test question 1", content: "test content 1")
+Question.create(id: 2, name: "Test name 2", title: " Test question 2", content: "test content 2")
+Question.create(id: 3, name: "Test name 3", title: " Test question 3", content: "test content 3")
+Question.create(id: 4, name: "Test name 4", title: " Test question 4", content: "test content 4")
+Question.create(id: 5, name: "Test name 5", title: " Test question 5", content: "test content 5")
